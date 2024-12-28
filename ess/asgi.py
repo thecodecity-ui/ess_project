8147aa73ac3daed80ef0f64a4802c6ef97908a9e
thecodecity-ui <thowfeekrahman123@gmail.com>
2024-12-28 15:34:27 +0530

    Add files via upload

---
+++ ess/asgi.py
@@ -0,0 +1,17 @@
+# your_project_name/asgi.py
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ess.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
